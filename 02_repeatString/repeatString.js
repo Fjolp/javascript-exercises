aadd2f3451ac7faf6b9798faa45ba024d4b0df5c
Fjolp <filippavicevic321@gmail.com>
2022-12-15 18:01:18 +0100

    done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) return 'ERROR';
+    let string = "";
+    for (let i = 0; i < num; i++) {
+        string += str;
+      }
+      return string;
 };
+
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
+repeatString('hey', -1);
+repeatString('hey', Number);
 
 // Do not edit below this line
 module.exports = repeatString;
