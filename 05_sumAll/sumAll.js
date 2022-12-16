903d2a2fa2ab164bd8db2c46765644eb20001300
Fjolp <filippavicevic321@gmail.com>
2022-12-16 18:59:42 +0100

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
 
-};
+    let sum = 0;
+    let finalSum = 0;
+
+    if(firstNum <0 || secondNum < 0 ) {
+        return 'ERROR'
+    }else if (typeof firstNum != 'number' || typeof secondNum != 'number'){
+        return 'ERROR'
+    }else if (firstNum > secondNum) {
+        for (let i = secondNum; i <= firstNum; i++) {
+            sum += i;
+    }}else{
+    for (let i = firstNum; i <= secondNum; i++) {
+        sum += i;
+    }};
+    return finalSum += sum;
+
+};  
 
 // Do not edit below this line
 module.exports = sumAll;
