ea17d48cd264db49a13a2b4910f1fe7b7ea78050
Fjolp <filippavicevic321@gmail.com>
2022-12-16 19:49:30 +0100

    done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if ((year % 4) === 0 && (year % 400 === 0 || year % 100 !== 0)) {
+        let isLeap = true;
+        return isLeap
+    }else{
+        isLeap = false;
+        return isLeap;
+    }
+    
 };
 
 // Do not edit below this line
