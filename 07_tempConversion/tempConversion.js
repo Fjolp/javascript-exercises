f0173a49deea0ecce01d9ccfe8fb40f96778b94d
Fjolp <filippavicevic321@gmail.com>
2022-12-20 19:13:25 +0100

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenhite) {
+
+  return Math.round((fahrenhite - 32) * (5/9) *10 ) /10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  return Math.round(((celcius * 9/5) + 32) *10 ) /10;
 };
 
 // Do not edit below this line
