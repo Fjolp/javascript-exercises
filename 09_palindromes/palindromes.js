ed2db2e4e31c78326df25283637a480fc5ecacc4
Fjolp <filippavicevic321@gmail.com>
2022-12-25 20:28:49 +0100

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let finalStr2 = str.toLowerCase().replace(/[^a-z]/g, "");
+    let str2 = str.toLowerCase().replace(/[^a-z]/g, "");
+    let myArray = str2.split("");
+    let reversed = myArray.reverse();
+    let reversedStr = reversed.join("");
+    
+    
+    if (reversedStr === finalStr2){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
