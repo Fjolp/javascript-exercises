d0777271a655f0944575299f8da95096bd4acbde
Fjolp <filippavicevic321@gmail.com>
2022-12-29 19:57:02 +0100

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    var trueNum = parseInt(num);
+
+    if(trueNum < 0){
+        return "OOPS"
+    }else{
+        let n1 = 0;
+        let n2 = 1;
+        let tempNum
+
+        for(var i = 1; i <= trueNum; i ++){
+            tempNum = n1 + n2;
+            n1 = n2;
+            n2 = tempNum;
+            }
+        return n1;
+    }
 };
 
 // Do not edit below this line
